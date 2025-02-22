c8de9ed4c89b4a168dad9d84b0ac7efcd4444f75
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-22 03:21:47 -0800

    added file

--- src/pokemon file/Pokemon.jsx
+++ src/pokemon file/Pokemon.jsx
@@ -5,7 +5,24 @@
 
 function Pokemon() {
     return (
-        <div>
+        <div className=" m-16 ">
+
+            <div>
+                <div className=" flex justify-center items-center gap-2">
+                    <img className=" h-[60px] " src="https://cdn-icons-png.flaticon.com/128/188/188970.png" alt="" />
+                    <p className=" text-center text-6xl text-black font-serif ">Pokemon Power</p>
+                </div>
+
+
+
+
+
+
+
+
+
+
+            </div>
 
         </div>
     )
