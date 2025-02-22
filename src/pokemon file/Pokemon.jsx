bc26369d5437db97102a17ef8a21524cf89ad712
minhazapon <minhazaponcodingwarrior@gmail.com>
2025-02-21 21:10:26 -0800

    added file

--- src/pokemon file/Pokemon.jsx
+++ src/pokemon file/Pokemon.jsx
@@ -1,6 +1,8 @@
 
 //work api https://pokeapi.co/api/v2/pokemon?limit=124
 
+//github https://github.com/thapatechnical/pokemon_thapa_react/blob/master/src/Pokemon.jsx
+
 function Pokemon() {
     return (
         <div>
